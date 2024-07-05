394c6d30b844c1bdc3e10e8a9ebb9100c2981245
Tegar Aditya <tegaraditya53@gmail.com>
2024-07-05 10:17:54 +0700

    feat: online check wrapper

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,28 @@
 <template>
+    <div class="absolute z-50 w-full" v-if="!isOnline">
+        <div class="w-full flex justify-center items-center">
+            <Message severity="error" :closable="false">Tidak ada koneksi internet</Message>
+        </div>
+    </div>
     <div class="bg-blue-50 dark:bg-gray-900" @contextmenu.prevent>
-        <slot />
+        <BlockUI :blocked="!isOnline" :auto-z-index="false" :base-z-index="40">
+            <slot />
+        </BlockUI>
     </div>
 </template>
+
+<script lang="ts" setup>
+import type { UserData } from '~/types/response';
+
+const isOnline = useOnline();
+const isLoading = ref(true);
+const local = (key: string) => localStorage.getItem(key);
+
+onMounted(async () => {
+    const formData: UserData = JSON.parse(local('formData') ?? '');
+
+    console.log(formData)
+
+    isLoading.value = false;
+});
+</script>
