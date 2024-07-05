91a62284aadcf0f79c2fe038d12f077ff1fbf654
Tegar Aditya <tegaraditya53@gmail.com>
2024-07-05 10:21:22 +0700

    adding page header

--- pages/test/appBeta.vue
+++ pages/test/appBeta.vue
@@ -1,3 +1,15 @@
 <template>
     <StemContainerApi />
 </template>
+
+<script lang="ts" setup>
+useHead({
+    title: 'Testlet Beta',
+    meta: [
+        {
+            name: 'description',
+            content: 'Halaman Test',
+        },
+    ],
+});
+</script>
