62498f56378d330bb06bbbcfb7d8f4efb8fd0f55
Tegar Aditya <tegaraditya53@gmail.com>
2024-07-05 10:21:40 +0700

    feat: adding question list

---
+++ components/StemNavigationWrapper.vue
@@ -0,0 +1,23 @@
+<template>
+    <Card class="w-full overflow-hidden max-w-xl mx-2">
+        <template #title> <h1 class="font-bold">Daftar Bacaan</h1></template>
+        <template #subtitle>Klik pada nomor untuk menuju nomor tersebut</template>
+        <template #content>
+            <ScrollPanel style="width: 100%; height: 300px">
+                <slot />
+                <ScrollTop target="parent" :threshold="100" icon="pi pi-arrow-up" />
+            </ScrollPanel>
+        </template>
+        <template #footer>
+            <div class="flex gap-3 mt-1">
+                <Button label="Kembali" severity="secondary" class="w-full" @click="show" />
+            </div>
+        </template>
+    </Card>
+</template>
+
+<script lang="ts" setup>
+const emit = defineEmits(['trigger']);
+
+const show = () => emit('trigger');
+</script>
